016083f8e6e447949f128d2dafe8fab98bb1a93c
Joseph Kariuki <joehene@gmail.com>
2024-03-17 21:06:16 +0300

    Created docker development configurations

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "Waiting for postgres..."
+
+while ! nc -z docid-db 5432; do
+    sleep 0.1
+done
+
+echo "PostgreSQL started"
+
+
+python manage.py recreate_db
+python manage.py run -h 0.0.0.0
